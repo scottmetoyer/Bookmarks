ad625e82377c2908c9619c916477a594a7a9a355
smetoyer <scott.metoyer@gmail.com>
2011-10-24 18:10:58 -0700

    New files added

---
+++ Bookmarks.UnitTests/BookmarkEditing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Bookmarks.Controllers;
+using Bookmarks.Models;
+
+namespace Bookmarks.UnitTests
+{
+    [TestFixture]
+    public class BookmarkEditing
+    {
+        [Test]
+        public void Can_Edit_Bookmark()
+        {
+            // Arrange
+            BookmarksController controller = new BookmarksController(UnitTestHelpers.MockBookmarkRepository());
+
+            // Act
+            var result = controller.Edit(3);
+
+            // Assert
+            result.ShouldBeDefaultView();
+            ((BookmarkViewModel)result.Model).Bookmark.Name.ShouldEqual("b3");
+            ((BookmarkViewModel)result.Model).Tags.Count.ShouldEqual(3);
+        }
+    }
+}
---
+++ Bookmarks/Infrastructure/StringSplitModelBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookmarks.Infrastructure
+{
+    public class StringSplitModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ValueProvider.GetValue(bindingContext.ModelName) == null)
+            {
+                return new string[] { };
+            }
+
+            string attemptedValue = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).AttemptedValue;
+            return !String.IsNullOrEmpty(attemptedValue) ? attemptedValue.Split(',') : new string[] { };
+        }
+    }
+}
